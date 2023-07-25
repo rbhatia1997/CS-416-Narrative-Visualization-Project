cc6395231fc3c2b37bfe0f46082dc578a0dfdb11
rbhatia <rbhatia@hmc.edu>
2023-07-25 13:54:41 -0700

    Prelim work

---
+++ scene1.js
@@ -0,0 +1,40 @@
+// Scene 1: Introduction
+
+// Function to create the overview of the dataset and its variables
+function createOverview() {
+  const overviewContainer = d3.select(".overview");
+
+  const overviewText = `Welcome to the Airline Safety Narrative Visualization. This interactive web page aims to explore the safety records of different airlines based on incidents and accidents that occurred during two time periods: 1985-1999 and 2000-2014. It aims to answer if there is a <strong> correlation between airline and crash risk.</strong>`;
+
+  const datasetVariables = [
+    "airline: Airline name (asterisk indicates that regional subsidiaries are included)",
+    "avail_seat_km_per_week: Available seat kilometers flown every week",
+    "incidents_85_99: Total number of incidents, 1985–1999",
+    "fatal_accidents_85_99: Total number of fatal accidents, 1985–1999",
+    "fatalities_85_99: Total number of fatalities, 1985–1999",
+    "incidents_00_14: Total number of incidents, 2000–2014",
+    "fatal_accidents_00_14: Total number of fatal accidents, 2000–2014",
+    "fatalities_00_14: Total number of fatalities, 2000–2014",
+  ];
+
+  overviewContainer.append("p").html(overviewText);
+
+  overviewContainer
+    .append("p")
+    .text("The dataset contains the following variables:")
+    .append("ul")
+    .selectAll("li")
+    .data(datasetVariables)
+    .enter()
+    .append("li")
+    .html((d) => d);
+
+  overviewContainer
+    .append("p")
+    .html(
+      `Data Source: This data is taken from / references the <a href="https://github.com/fivethirtyeight/data/blob/master/airline-safety/airline-safety.csv" target="_blank" rel="noopener">following dataset</a>`
+    );
+}
+
+// Call the function to create the overview when the document is loaded
+document.addEventListener("DOMContentLoaded", createOverview);
---
+++ scene2.js
@@ -0,0 +1,157 @@
+// Data URL (replace 'path_to_your_data_file.csv' with the actual path)
+const dataURL = "airline-safety.csv";
+
+// Function to draw the bar chart
+function drawChart(selectedAirline) {
+  // Load the data
+  d3.csv(dataURL).then((data) => {
+    // Filter data for the selected airline
+    const selectedAirlineData = data.find((d) => d.airline === selectedAirline);
+
+    // Check if the selected airline data exists
+    if (selectedAirlineData) {
+      // Clear previous chart
+      d3.select("#chart").selectAll("*").remove();
+
+      // Set up the chart dimensions
+      // Set up the chart dimensions
+      const margin = { top: 60, right: 20, bottom: 100, left: 60 };
+      const width = 600 - margin.left - margin.right;
+      const height = 500 - margin.top - margin.bottom;
+
+      // Create the SVG container
+      const svg = d3
+        .select("#chart")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+      // Data for the selected airline
+      const airlineData = [
+        {
+          label: "Incidents",
+          value: selectedAirlineData.incidents_85_99,
+          color: "#1f77b4",
+        },
+        {
+          label: "Fatal Accidents",
+          value: selectedAirlineData.fatal_accidents_85_99,
+          color: "#ff7f0e",
+        },
+        {
+          label: "Fatalities",
+          value: selectedAirlineData.fatalities_85_99,
+          color: "#d62728",
+        },
+      ];
+
+      // X scale
+      const xScale = d3
+        .scaleBand()
+        .domain(airlineData.map((d) => d.label))
+        .range([0, width])
+        .paddingInner(0.2)
+        .paddingOuter(0.2);
+
+      // Y scale
+      // Find the maximum value of fatalities among all airlines
+      const maxFatalities = d3.max(data, (d) => d.fatalities_85_99);
+
+      // Y scale
+      const yScale = d3
+        .scaleLinear()
+        .domain([0, maxFatalities * 1.2])
+        .range([height, 0]);
+
+      // Draw the bars
+      svg
+        .selectAll(".bar")
+        .data(airlineData)
+        .enter()
+        .append("rect")
+        .attr("class", "bar")
+        .attr("x", (d) => xScale(d.label))
+        .attr("y", (d) => yScale(d.value))
+        .attr("width", xScale.bandwidth())
+        .attr("height", (d) => height - yScale(d.value))
+        .attr("fill", (d) => d.color)
+        .on("mouseover", function (event, d) {
+          // Show tooltip on hover
+          const tooltip = d3.select("#tooltip");
+          tooltip
+            .html(`<strong>${d.label}:</strong> ${d.value}`)
+            .style("left", `${event.pageX}px`)
+            .style("top", `${event.pageY - 28}px`)
+            .style("visibility", "visible");
+        })
+        .on("mouseout", function () {
+          // Hide tooltip when not hovering
+          const tooltip = d3.select("#tooltip");
+          tooltip.style("visibility", "hidden");
+        });
+
+      // X axis
+      svg
+        .append("g")
+        .attr("transform", `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
+
+      // Y axis
+      svg.append("g").call(d3.axisLeft(yScale));
+
+      // Add a title
+      svg
+        .append("text")
+        .attr("x", width / 2)
+        .attr("y", -5)
+        .attr("text-anchor", "middle")
+        .text(`Safety Records for ${selectedAirline} (1985-1999)`);
+
+      // Add a tooltip container
+      d3.select("body")
+        .append("div")
+        .attr("id", "tooltip")
+        .style("position", "absolute")
+        .style("background", "#f7f7f7")
+        .style("padding", "5px")
+        .style("border", "1px solid #ccc")
+        .style("border-radius", "5px")
+        .style("font-size", "12px")
+        .style("visibility", "hidden");
+    }
+  });
+}
+
+// Function to handle the dropdown selection change
+function handleDropdownChange() {
+  const selectedAirline = d3.select("#airlineSelect").property("value");
+  if (selectedAirline) {
+    drawChart(selectedAirline);
+  }
+}
+
+// Function to handle the next scene button click
+function handleNextSceneClick() {
+  // Navigate to the next scene (replace 'scene3.html' with the next scene's HTML file)
+  window.location.href = "scene3.html";
+}
+
+// Event listeners
+d3.select("#airlineSelect").on("change", handleDropdownChange);
+d3.select("#nextSceneButton").on("click", handleNextSceneClick);
+
+// Load the data and populate the dropdown options
+d3.csv(dataURL).then((data) => {
+  const airlines = data.map((d) => d.airline);
+  d3.select("#airlineSelect")
+    .selectAll("option")
+    .data(airlines)
+    .enter()
+    .append("option")
+    .text((d) => d)
+    .attr("value", (d) => d);
+
+  // Initial call to draw the chart with the default selected airline
+  drawChart(airlines[0]);
+});
